21d147500587fe40b70c839c77ae877f2ba03ec5
Srikanth Vavilapalli <srikanth.vavilapalli@ericsson.com>
2016-09-02 01:37:34 +0000

    Ceilometer integrtion files to be installed at broadview collector module
    Change-Id: I3467b9ffa1f8e86f4865332df8190c4bf43efd27

---
+++ xos/synchronizer/ceilometer/broadview_collector_plugin/plugins/ceilometerpublisher.py
@@ -0,0 +1,100 @@
+# (C) Copyright Broadcom Corporation 2016
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from broadviewpublisherbase import BroadViewPublisherBase
+from kombu.connection import BrokerConnection
+from kombu.messaging import Exchange, Queue, Consumer, Producer
+import six
+import uuid
+import datetime
+from broadview_collector.serializers.bst_to_ceilometer import BSTToCeilometer
+import json
+import ConfigParser
+import sys
+
+try:
+    from oslo_log import log 
+except:
+    import logging as log
+
+LOG = log.getLogger(__name__)
+
+class BroadViewPublisher(BroadViewPublisherBase):
+    def readConfig(self):
+        try:
+            bvcfg = ConfigParser.ConfigParser()
+            bvcfg.read("/etc/broadviewcollector.conf")
+            self.rabbit_user = bvcfg.get("ceilometer", "rabbit_user")
+            self.rabbit_password = bvcfg.get("ceilometer", "rabbit_password") 
+            self.rabbit_host = bvcfg.get("ceilometer", "rabbit_host") 
+            self.rabbit_exchange = bvcfg.get("ceilometer", "rabbit_exchange") 
+        except:
+            LOG.info("BroadViewPublisher: unable to read configuration")
+
+    def errback(self, exc, interval):
+        LOG.error('Error: %r', exc, exc_info=1)
+        LOG.info('Retry in %s seconds.', interval)
+   
+    def setup_rabbit_mq_channel(self):
+        ceilometer_exchange = Exchange(self.rabbit_exchange, "topic", durable=False)
+        # connections/channels
+        connection = BrokerConnection(self.rabbit_host, self.rabbit_user, self.rabbit_password)
+        LOG.info("BroadViewPublisher: Connection to RabbitMQ server successful")
+        channel = connection.channel()
+        # produce
+        self._producer = Producer(channel, exchange=ceilometer_exchange, routing_key='notifications.info')
+        self._publish = connection.ensure(self._producer, self._producer.publish, errback=self.errback, max_retries=3)
+
+    def __init__(self):
+        self.rabbit_user = 'openstack'
+        self.rabbit_password = 'password'
+        self.rabbit_host = '1.2.3.4'
+        self.rabbit_exchange = 'broadview_service'
+        self._producer = None
+        self._publish = None
+        self.readConfig()
+        LOG.info("BroadViewPublisher: Ceilometer Publisher Initialized")
+
+    def publish(self, host, data):
+        code = 500
+        #  get a producer if needed
+        if not self._producer:
+            self.setup_rabbit_mq_channel()
+        if self._producer: 
+            code = 200
+        if self.isBST(data):
+            success, sdata = BSTToCeilometer().serialize(host, data)
+        #elif self.isPT(data):
+        #    self._topic = "broadview-pt"
+        #    success, sdata = PTToMonasca().serialize(host, data)
+        else:
+            success = False
+        if success:
+            sdata = json.loads(sdata)
+            if success: 
+                for x in sdata:
+                    try:
+                        #self._producer.publish(x)
+                        self._publish(x)
+                        LOG.debug("BroadViewPublisher: Sent data to ceilometer exchange")
+                    except Exception as e:
+                        LOG.info('exception: {}'.format(e))
+                        LOG.info('unable to send to ceilometer exchange {}: {}'.format(self.rabbit_exchange, sys.exc_info()[0]))
+            else:
+                code = 500
+        return code
+
+    def __repr__(self):
+        return "BroadView Ceilometer Publisher {} {}".format(self.rabbit_host, self.rabbit_exchange) 
+
---
+++ xos/synchronizer/ceilometer/ceilometer-plugins/network/ext_services/broadview/notifications.py
@@ -0,0 +1,66 @@
+#
+# Copyright 2012 New Dream Network, LLC (DreamHost)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Handler for producing network counter messages from Neutron notification
+   events.
+
+"""
+
+import oslo_messaging
+from oslo_config import cfg
+
+from ceilometer.agent import plugin_base
+from oslo_log import log
+from ceilometer import sample
+
+OPTS = [
+    cfg.StrOpt('broadview_control_exchange',
+               default='broadview_service',
+               help="Exchange name for Broadview notifications."),
+]
+
+cfg.CONF.register_opts(OPTS)
+
+LOG = log.getLogger(__name__)
+
+
+class BroadViewNotificationBase(plugin_base.NotificationBase):
+
+    resource_name = None
+    event_types = ['broadview.bst.*']
+
+    def get_targets(self,conf):
+        """Return a sequence of oslo.messaging.Target
+
+        This sequence is defining the exchange and topics to be connected for
+        this plugin.
+        """
+        LOG.info("SRIKANTH: get_targets for BroadView Notification Listener")
+        return [oslo_messaging.Target(topic=topic,
+                                      exchange=conf.broadview_control_exchange)
+                for topic in self.get_notification_topics(conf)]
+
+    def process_notification(self, message):
+        if message['payload']:
+            resource_id = 'broadview_' + message["payload"]["asic-id"] 
+            LOG.info('SRIKANTH: Received BroadView %(event_type)s notification: resource_id=%(resource_id)s' % {'event_type': message['event_type'], 'resource_id': resource_id})
+            yield sample.Sample.from_notification(
+                name=message['event_type'],
+                type=sample.TYPE_GAUGE,
+                unit='bv-agent',
+                volume=message['payload']['value'],
+                user_id=None,
+                project_id='default_admin_tenant',
+                resource_id=resource_id,
+                message=message)
