7e9f4d33b0d00ff621a058c357c968eaf62b9afa
Sapan Bhatia <gwsapan@gmail.com>
2017-01-24 19:32:59 +0100

    CORD-706: Make room for new Ansible-invocation interface
    Change-Id: I3a3e83d293d5967e5a2ad1326638c221b7f398c6

--- xos/synchronizer/steps/sync_sflowtenant.py
+++ xos/synchronizer/steps/sync_sflowtenant.py
@@ -10,7 +10,7 @@ import json
 from django.db.models import F, Q
 from xos.config import Config
 from synchronizers.base.syncstep import SyncStep
-from synchronizers.base.ansible import run_template_ssh
+from synchronizers.base.ansible_helper import run_template_ssh
 from synchronizers.base.SyncInstanceUsingAnsible import SyncInstanceUsingAnsible
 from core.models import Service, Slice, ControllerSlice, ControllerUser
 from services.monitoring.models import SFlowService, SFlowTenant
