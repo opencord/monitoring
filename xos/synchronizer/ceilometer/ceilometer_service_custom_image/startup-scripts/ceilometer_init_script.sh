eb0d11c7455b5ad98f6d8228cf687e7c9277bb2e
Matteo Scandolo <matteo.scandolo@gmail.com>
2017-08-08 13:05:26 -0700

    [CORD-1685] Adding copyright informations
    Change-Id: Id035a8f8d02432c9dfc7aab6457976d9868212fc

--- xos/synchronizer/ceilometer/ceilometer_service_custom_image/startup-scripts/ceilometer_init_script.sh
+++ xos/synchronizer/ceilometer/ceilometer_service_custom_image/startup-scripts/ceilometer_init_script.sh
@@ -1,3 +1,19 @@
+
+# Copyright 2017-present Open Networking Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 STARTUP_PATH="/home/ubuntu/monitoring/xos/synchronizer/ceilometer/ceilometer_service_custom_image/startup-scripts"
 IP=$(ifconfig | awk '/inet addr/{print substr($2,6)}' | grep "10.0.3")
 #IP=$(ifconfig | awk '/inet addr/{print substr($2,6)}' | grep "10.11.10")
